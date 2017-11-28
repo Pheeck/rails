473be29952021ec44617da1e67a79750cc64a2ec
Filip Kastl <filip.kastl@gmail.com>
2017-11-28 16:31:47 +0100

    Track and Rail classes done

--- battle_ground.gd
+++ battle_ground.gd
@@ -11,16 +11,6 @@ extends Node2D
 var Track = preload("track.gd")
 
 
-# Generating
-var min_branch = 5  # How long should the branches be at minimum; Putting in 0 breaks things
-var margin = 1      # How many nodes from the rim of the grid should be left free
-var graph = [       # Graph respresenting relations between rails; Track is generated from it
-	[1, 0],
-	[2, 0],
-	[0, 0]
-]
-
-
 # Pregenerated
 var track = [
 	[
@@ -46,6 +36,7 @@ var track = [
 
 # Graphics
 var gap = 64
+var margin = 1  # Counted in gaps
 var track_width = 5.0
 var colors = [
 	Color(1.0, 0.5, 0.5),  # Red
@@ -70,175 +61,6 @@ func reduce_vector(vector):
 	return vector
 
 
-# Generating
-func get_neighbors(nodes, x, y):
-	""" Returns array of positions of neighbors of a node at xy position """
-	var neighbors = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
-	var result = []
-	for neighbor in neighbors:  # Check if neighbors are valid
-		if not (neighbor[0] >= nodes.size() or neighbor[1] >= nodes[0].size() or
-				neighbor[0] < 0 or neighbor[1] < 0):
-			result.append(neighbor)
-	return result
-
-func get_max_value_neighbors(nodes, x, y):
-	""" Returns array of positions of neighbors of a node at xy; Only those with the highest value are returned """
-	var neighbors = get_neighbors(nodes, x, y)
-	var max_value = 0
-	var result = []
-	for neighbor in neighbors:
-		var value = nodes[neighbor[0]][neighbor[1]]
-		if value > max_value:
-			result = [neighbor]
-			max_value = value
-		elif value == max_value:
-			result.append(neighbor)
-	return result
-
-func set_neighbor_nodes(nodes, x, y, value):
-	""" Sets neighbor nodes values to a value; Ignores nodes with a smaller value """
-	for neighbor in get_neighbors(nodes, x, y):
-		x = neighbor[0]
-		y = neighbor[1]
-		if not nodes[x][y] <= value:  # Ignore smaller value nodes
-			nodes[x][y] = value
-
-func clear_nodes(nodes):
-	""" Clear values (give those nodes 2147483647) in a 2d node array (0 stays) """
-	var x = 0
-	while x < nodes.size():
-		var y = 0
-		while y < nodes[0].size():
-			if nodes[x][y] != 0:
-				nodes[x][y] = 2147483647
-			y += 1
-		x += 1
-
-func evaluate_nodes(nodes):
-	""" Assign values to unoccupied nodes in a 2d node array """
-	clear_nodes(nodes)      # If nodes already had some values, it would mess up the evaluation process
-	
-	var keep_going = true   # This signalizes that there is still work to do
-	var solving_value = 0   # The value of nodes whose neighbors we're evaluating
-	while keep_going:
-		keep_going = false  # If no operation is done this cycle, there is nothing to do
-		var x = 0
-		while x < nodes.size():
-			var y = 0
-			while y < nodes[0].size():
-				var curr_value = nodes[x][y]
-				if curr_value == solving_value:  # Found a node whose neighbors we're evaluating
-					set_neighbor_nodes(nodes, x, y, solving_value + 1)
-					keep_going = true  # We have done something
-				y += 1
-			x += 1
-		solving_value += 1  
-
-func generate_track(graph, node_count_x, node_count_y):
-	""" Generate a track from a graph of rail realtions """
-	var rail = []
-	
-	# Assure it's int
-	node_count_x = int(node_count_x)
-	node_count_y = int(node_count_y)
-	
-	# Subtract margin from the count of available nodes
-	node_count_x -= (margin * 2) - 1  # We can go all the way right, but not left
-	node_count_y -= (margin * 2) - 1  # To put it differently: we can go to max_valueimal x or y but not to 0 x or y
-	
-	# For storing values of nodes; Exceptions: 0 = occupied, 2147483647 = uninitialized
-	var nodes = []
-	for i in range(node_count_x):  # Fill nodes array with 2147483647
-		var a = []
-		for j in range(node_count_y):
-			a.append(2147483647)
-		nodes.append(a)
-	
-	# Generate a random position; That will be the rail origin
-	var origin_x = (randi() % (node_count_x - 2)) + 1
-	var origin_y = (randi() % (node_count_y - 2)) + 1
-	
-	# Create the base; Either...
-	if randi() % 2:  # ...horizontally
-		rail.append(Vector2(origin_x - 1, origin_y))
-		nodes[origin_x - 1][origin_y] = 0
-		# rail.append(Vector2(origin_x, origin_y))
-		nodes[origin_x][origin_y] = 0
-		rail.append(Vector2(origin_x + 1, origin_y))
-		nodes[origin_x + 1][origin_y] = 0
-	else:            # ...or vertically
-		rail.append(Vector2(origin_x, origin_y - 1))
-		nodes[origin_x][origin_y - 1] = 0
-		# rail.append(Vector2(origin_x, origin_y))
-		nodes[origin_x][origin_y] = 0
-		rail.append(Vector2(origin_x, origin_y + 1))
-		nodes[origin_x][origin_y + 1] = 0
-	
-	# Evaluate nodes (+ print the result, pls delet)
-	evaluate_nodes(nodes)
-	var y = 0
-	while y < nodes[0].size():
-		var x = 0
-		var d = []
-		var s = ""
-		while x < nodes.size():
-			d.append(nodes[x][y])
-			s += "%4d "
-			x += 1
-		print(s % d)
-		y += 1
-	
-	# Grow a branch (testing)
-	var curr_node = [rail[1].x, rail[1].y]  # Start with the last node in the base
-	var count = 0
-	while count < min_branch:
-		# Get the neighbors with max value
-		var neighbors = get_max_value_neighbors(nodes, curr_node[0], curr_node[1])
-		# Choose a random neighbor and add him to the branch
-		curr_node = neighbors[randi() % neighbors.size()]
-		rail.append(Vector2(curr_node[0], curr_node[1]))
-		nodes[curr_node[0]][curr_node[1]] = 0
-		# We now continue the cycle again, but on the chosen node
-		count += 1
-		
-	
-	# Evaluate nodes (+ print the result, pls delet)
-	print("---")
-	evaluate_nodes(nodes)
-	var y = 0
-	while y < nodes[0].size():
-		var x = 0
-		var d = []
-		var s = ""
-		while x < nodes.size():
-			d.append(nodes[x][y])
-			s += "%4d "
-			x += 1
-		print(s % d)
-		y += 1
-	
-	# Grow a branch (testing)
-	var curr_node = [rail[0].x, rail[0].y]  # Start with the last node in the base
-	var count = 0
-	while count < min_branch:
-		# Get the neighbors with max value
-		var neighbors = get_max_value_neighbors(nodes, curr_node[0], curr_node[1])
-		print(neighbors)
-		# Choose a random neighbor and add him to the branch
-		curr_node = neighbors[randi() % neighbors.size()]
-		rail.insert(0, Vector2(curr_node[0], curr_node[1]))
-		nodes[curr_node[0]][curr_node[1]] = 0
-		# We now continue the cycle again, but on the chosen node
-		count += 1
-	
-	####
-	var count = 0
-	while count < rail.size():
-		rail[count].x += margin
-		rail[count].y += margin
-		count += 1
-	return [rail]
-
 # Graphics
 func draw_grid(gap):
 	var screen_size = get_viewport().get_rect().size
@@ -254,6 +76,10 @@ func draw_grid(gap):
 		y += gap
 
 func draw_section(node1, node2, color):
+	node1.x += margin
+	node2.x += margin
+	node1.y += margin
+	node2.y += margin
 	draw_line(node1 * gap, node2 * gap, color, track_width)
 	
 func draw_start(node1, node2, color):
@@ -271,19 +97,21 @@ func draw_end(node1, node2, color):
 	draw_section(node1, node2, color)
 
 func draw_rail(rail, color):
-	var node1 = rail[0]
-	var node2 = rail[1]
+	var vectors = rail.get_vectors()
+	
+	var node1 = vectors[0]
+	var node2 = vectors[1]
 	draw_start(node1, node2, color)
 	
 	var count = 2  # We'd normally start at one and we've already gone throught one node
-	while count < rail.size() - 1:
+	while count < vectors.size() - 1:
 		node1 = node2
-		node2 = rail[count]
+		node2 = vectors[count]
 		draw_section(node1, node2, color)
 		count += 1
 		
 	var node1 = node2
-	var node2 = rail[count]
+	var node2 = vectors[count]
 	draw_end(node1, node2, color)
 
 func draw_track(track):
@@ -299,5 +127,4 @@ func _ready():
 	
 func _draw():
 	draw_grid(gap)
-	# draw_track(generate_track(graph, 16, 9))
-	draw_track(Track(graph, 16, 9))
+	draw_track(Track.new([], 15, 9))  # TODO: Graph
--- track.gd
+++ track.gd
@@ -25,7 +25,7 @@ func _init(graph, node_count_x, node_count_y):
 		nodes.append(a)
 	
 	# Create rail (testing)
-	rails.append(Rail())
+	rails.append(Rail.new(self))
 
 func get_neighbors(pos_x, pos_y):
 	""" Returns array of positions of neighbors of a node at pos position """
@@ -64,8 +64,8 @@ func get_max_value_neighbors(pos_x, pos_y):
 func set_neighbor_nodes(pos_x, pos_y, value):
 	""" Sets neighbor nodes values to a value; Ignores nodes with a smaller value """
 	for neighbor in get_neighbors(pos_x, pos_y):
-		x = neighbor[0]
-		y = neighbor[1]
+		var x = neighbor[0]
+		var y = neighbor[1]
 		if not nodes[x][y] <= value:  # Ignore smaller value nodes
 			nodes[x][y] = value
 
@@ -80,6 +80,26 @@ func clear_nodes():
 			y += 1
 		x += 1
 
+func evaluate_nodes():
+	""" Assign values to unoccupied nodes in nodes array """
+	clear_nodes()           # If nodes already had some values, it would mess up the evaluation process
+	
+	var keep_going = true   # This signalizes that there is still work to do
+	var solving_value = 0   # The value of nodes whose neighbors we're evaluating
+	while keep_going:
+		keep_going = false  # If no operation is done this cycle, there is nothing to do
+		var x = 0
+		while x < nodes.size():
+			var y = 0
+			while y < nodes[0].size():
+				var curr_value = nodes[x][y]
+				if curr_value == solving_value:  # Found a node whose neighbors we're evaluating
+					set_neighbor_nodes(x, y, solving_value + 1)
+					keep_going = true  # We have done something
+				y += 1
+			x += 1
+		solving_value += 1
+
 
 # -- Inner classes --
 
@@ -92,8 +112,8 @@ class Rail:
 		self.track = track
 		
 		# Generate a random position; That will be the path origin
-		var origin_x = (randi() % (track.nodes.size - 2)) + 1  # We generate the position so that the origin can't be on the edge of nodes array
-		var origin_y = (randi() % (track.nodes[0].size - 2)) + 1
+		var origin_x = (randi() % (track.nodes.size() - 2)) + 1  # We generate the position so that the origin can't be on the edge of nodes array
+		var origin_y = (randi() % (track.nodes[0].size() - 2)) + 1
 		
 		# Create the base; Either...
 		if randi() % 2:  # ...horizontally
@@ -112,15 +132,18 @@ class Rail:
 			track.nodes[origin_x][origin_y + 1] = 0
 			
 		# Grow branches from each side
-		path = path + grow_branch(path[1])           # From the back
-		path = grow_branch(path[0]).invert() + path  # From the front
+		var new_branch = grow_branch(path[1][0], path[1][1])
+		path = path + new_branch  # From the back
+		new_branch = grow_branch(path[0][0], path[0][1])
+		new_branch.invert()       # We need to connect it to the path by the node it grew from
+		path = new_branch + path  # From the front
 	
 	func grow_branch(start_x, start_y):  # TODO: More different types of growing; Anticipating bad branches
 		""" Grows a branch and returns its path """
 		var result = []
 		
 		# We will be using node valus -> we need to evaluate them first
-		evaluate_nodes()
+		track.evaluate_nodes()
 		
 		# Start with the starting node
 		var curr_node = [start_x, start_y]
@@ -130,7 +153,7 @@ class Rail:
 		var count = 0
 		while count < track.min_branch:
 			# Get the neighbors with max value
-			var neighbors = get_max_value_neighbors(track.nodes, curr_node[0], curr_node[1])
+			var neighbors = track.get_max_value_neighbors(curr_node[0], curr_node[1])
 			
 			# Choose a random neighbor and add him to the branch
 			curr_node = neighbors[randi() % neighbors.size()]
@@ -140,4 +163,14 @@ class Rail:
 			# We now continue the cycle again, but on the chosen node
 			count += 1
 		
+		return result
+	
+	func get_vectors():
+		""" Return this track's path with nodes converted to Vector2s """
+		var result = []
+		
+		for node in path:
+			var new_vector = Vector2(node[0], node[1])
+			result.append(new_vector)
+			
 		return result
